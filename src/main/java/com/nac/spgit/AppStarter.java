55085f38fa744dfa71ad7e96918be8b5c601b7e5
Smiji John <johnsmiji861@gmail.com>
2024-10-08 00:46:25 +0100

    Added the comment

--- src/main/java/com/nac/spgit/AppStarter.java
+++ src/main/java/com/nac/spgit/AppStarter.java
@@ -3,6 +3,7 @@ package com.nac.spgit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Mention the @Spring boot annotation
 @SpringBootApplication
 public class AppStarter {
     public static void main(String[] args) {
